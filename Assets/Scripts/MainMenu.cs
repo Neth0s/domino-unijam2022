2be0e9ecf032a85a8c63c91cfdf2d2ebea496970
CASTEL-Florian <106156391+CASTEL-Florian@users.noreply.github.com>
2022-11-19 21:39:51 +0100

    Main Menu fixed

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -57,7 +57,6 @@ public class MainMenu : MonoBehaviour
             rightArrow.SetActive(false);
         else
             rightArrow.SetActive(true);
-        print(currentLevelId);
         image.sprite = levels[currentLevelId].sprite;
     }
 
