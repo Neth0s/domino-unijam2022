6015816ab9ee808df4e754965ca8f916a5d263af
nethos <geeklego@gmail.com>
2022-11-19 11:53:23 +0100

    add domino physics

--- Assets/Scripts/Showdown.cs
+++ Assets/Scripts/Showdown.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Showdown : MonoBehaviour
 {
+    [SerializeField] private GameObject firstDomino;
+
+    [Header("Initial push")]
+    [SerializeField] private float torque = 10;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        rb = firstDomino.GetComponent<Rigidbody>();
+        rb.AddTorque(firstDomino.transform.right * torque);
     }
 }
